4eae337a9290576e63f023c5cfe10ff671f5b18d
Haniel <alu0100721690@ull.edu.es>
2014-10-16 16:11:15 +0100

    Clase Complejos Suma y Resta

---
+++ complejos.rb
@@ -0,0 +1,26 @@
+class Complejos_HYM
+
+    attr_reader :real, :img
+    #metodo inicializa el complejo
+     def initialize(real, img)
+        @real= real
+        @img = img
+     end         
+
+     def to_s
+        if @img > 0
+          "#{@real}+#{@img}i"
+        else
+          "#{@real}#{@img}i" 
+        end
+     end
+
+     def +(other)
+        Complejos_HYM.new(@real + other.real, @img + other.img)
+     end
+
+     def -(other)
+       Complejos_HYM.new(@real - other.real, @img - other.img)
+     end
+
+end
