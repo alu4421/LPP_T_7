cf1be484f903b9589467da089d26771693d77f44
Maria Rojas <alu0100605983@ull.edu.es>
2014-10-16 23:18:56 +0200

    Primeras pruebas, mostrado, suma y resta

---
+++ test/pruebas_unitarias.rb
@@ -0,0 +1,20 @@
+require "./complejos.rb"
+require 'test/unit'
+
+
+class Complejo_Test < Test::Unit::TestCase
+  ef setup
+    @c1 = Complejo.new(4,3)
+    @c2 = Complejo.new(1,1)
+  end
+  
+  def test_simple
+  
+    assert_equal("(4+3i)", @c1.to_s)		#Comprobamos que se muestre correctamente c1
+    assert_equal("(1+1i)", @c1.to_s) 		#Comprobamos que se muestre correctamente c1
+    assert_equal("(5+4i)", (@c1+@b).to_s)	#Comprobamos que se sume correctamente c1+c2
+    assert_equal("(3+2i)", (@c1-@b).to_s)	#Comprobamos que se reste correctamente c1-c2
+  end
+
+
+end 
