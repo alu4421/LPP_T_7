0a45c8693f4f137e6b59da40912b6a369d17e2fc
Maria Rojas <alu0100605983@ull.edu.es>
2014-10-17 12:38:35 +0200

    Terminados los tests

--- test/pruebas_unitarias.rb
+++ test/pruebas_unitarias.rb
@@ -10,10 +10,14 @@ class Complejo_Test < Test::Unit::TestCase
   
   def test_simple
   
-    assert_equal("(4+3i)", @c1.to_s)		#Comprobamos que se muestre correctamente c1
-    assert_equal("(1+1i)", @c1.to_s) 		#Comprobamos que se muestre correctamente c1
-    assert_equal("(5+4i)", (@c1+@b).to_s)	#Comprobamos que se sume correctamente c1+c2
-    assert_equal("(3+2i)", (@c1-@b).to_s)	#Comprobamos que se reste correctamente c1-c2
+    assert_equal("(4+3i)", @c1.to_s)				#Comprobamos que se muestre correctamente c1
+    assert_equal("(1+1i)", @c1.to_s) 				#Comprobamos que se muestre correctamente c1
+    assert_equal("(5+4i)", (@c1+@c2).to_s)			#Comprobamos que se sume correctamente c1+c2
+    assert_equal("(3+2i)", (@c1-@c2).to_s)			#Comprobamos que se reste correctamente c1-c2
+    assert_equal("(1+7i)", (@c1 * @C2).to_s)		#Comprobamos que multiplique correctamente c1*c2
+    assert_equal("(3.5+0.5i)", (@c1 / @C2).to_s)	#Comprobamos que divida correctamente c1/c2
+    assert_equal("(8+6i)",(@c1 x 2).to_s)			#Comprobamos que se multiplique correctamente c1*2
+
   end
 
 
