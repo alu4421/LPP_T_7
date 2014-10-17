05151d9f1e72eacc33cfc85766614e57db904f30
Haniel <alu0100721690@ull.edu.es>
2014-10-17 12:51:59 +0100

    Cambio de expresion productor escalar

--- test/pruebas_unitarias.rb
+++ test/pruebas_unitarias.rb
@@ -16,7 +16,7 @@ class Complejo_Test < Test::Unit::TestCase
     assert_equal("(3+2i)", (@c1-@c2).to_s)			#Comprobamos que se reste correctamente c1-c2
     assert_equal("(1+7i)", (@c1 * @C2).to_s)		#Comprobamos que multiplique correctamente c1*c2
     assert_equal("(3.5+0.5i)", (@c1 / @C2).to_s)	#Comprobamos que divida correctamente c1/c2
-    assert_equal("(8+6i)",(@c1 x 2).to_s)			#Comprobamos que se multiplique correctamente c1*2
+    assert_equal("(8+6i)",(@c1 & 2).to_s)			#Comprobamos que se multiplique correctamente c1*2
 
   end
 
